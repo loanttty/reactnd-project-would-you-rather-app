5239984795d1c39c46e28ed80f944c907b933f70
LoanTT <79430764+loanttty@users.noreply.github.com>
2021-05-13 18:24:33 +0800

    Initial Commit

---
+++ src/actions/shared.js
@@ -0,0 +1,22 @@
+import {receiveQuestions} from "./questions"
+import {receiveUsers} from "./users"
+import {setAuthedUser} from "./authedUser"
+import {_getQuestions, _getUsers} from "../utils/_DATA"
+
+const AUTHED_ID = "sarahedo"
+
+export function handleInitialUpdateStoreQuestions () {
+    return (dispatch) => {
+        return _getQuestions()
+                .then((questions) => dispatch(receiveQuestions(questions)))
+    }
+}
+export function handleInitialUpdateStoreUsers () {
+    return (dispatch) => {
+        return _getUsers()
+                .then((users) => dispatch(receiveUsers(users)))
+    }
+}
+export function handleInitialUpdateStoreAuthedUser () {
+    return (dispatch) => dispatch(setAuthedUser(AUTHED_ID))
+}
